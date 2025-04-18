7efefb0e298cb7f3532dc11d43df7fb340d95198
Tim Kluge <timklge@gmail.com>
2025-04-18 11:58:11 +0200

    Reset preferences if file is corrupted

--- app/src/main/kotlin/de/timklge/karooroutegraph/MainActivity.kt
+++ app/src/main/kotlin/de/timklge/karooroutegraph/MainActivity.kt
@@ -2,15 +2,21 @@ package de.timklge.karooroutegraph
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import de.timklge.karooroutegraph.screens.MainScreen
 import de.timklge.karooroutegraph.theme.AppTheme
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings", corruptionHandler = ReplaceFileCorruptionHandler {
+    Log.w(KarooRouteGraphExtension.TAG, "Error reading settings, using default values")
+    emptyPreferences()
+})
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
