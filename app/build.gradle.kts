323061a68f020f4d9ccf7cf38a4eb8944c14205b
Tim Kluge <timklge@gmail.com>
2025-11-23 19:21:10 +0100

    Update README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,13 +71,14 @@ tasks.register("generateManifest") {
             "latestVersionCode" to android.defaultConfig.versionCode,
             "developer" to "github.com/timklge",
             "description" to "Open-source extension that provides an elevation graph datafield depicting the complete current route, including climbs and POIs (e. g. checkpoints and refueling stops)",
-            "releaseNotes" to "* Fix horizontal elevation profile draw order",
+            "releaseNotes" to "* Add option to show surface conditions on the elevation graph",
             "screenshotUrls" to listOf(
+                "https://github.com/timklge/karoo-routegraph/releases/latest/download/horizontal_routegraph.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/minimap.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/poinav.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/vertical_routegraph.png",
+                "https://github.com/timklge/karoo-routegraph/releases/latest/download/routegraph_surface_conditions.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/chevrons.png",
-                "https://github.com/timklge/karoo-routegraph/releases/latest/download/horizontal_routegraph.png",
             ),
         )
 
