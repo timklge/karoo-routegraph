45a37690809577f4d86b3bd4ee20446391c6a738
Tim Kluge <timklge@gmail.com>
2026-05-10 21:49:15 +0200

    Update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ tasks.register("generateManifest") {
             "latestVersionCode" to android.defaultConfig.versionCode,
             "developer" to "github.com/timklge",
             "description" to "Open-source extension that provides an elevation graph datafield depicting the complete current route, including climbs and POIs (e. g. checkpoints and refueling stops). Also provides a minimap datafield and a POI navigation datafield to look up upcoming POIs of certain categories on the route (e. g. supermarkets).",
-            "releaseNotes" to "* Fix auto-added POIs are refreshed too often when no route is loaded\n* Fix offline POI provider truncates unsorted search results\n* Add additional POI categories, remove bench category",
+            "releaseNotes" to "* Sort poi categories by most recently used\n* Move tab bar to the top of the screen on POI screen\n* Fix last known position on route POI is not shown on custom POI tab",
             "screenshotUrls" to listOf(
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/horizontal_routegraph.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/minimap.png",
