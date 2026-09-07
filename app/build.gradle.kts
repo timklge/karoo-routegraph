4ef80717b6b3ba02f873de4f1b8ea3f47e1bdbe8
Tim Kluge <timklge@gmail.com>
2026-09-07 22:42:33 +0200

    Update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ tasks.register("generateManifest") {
             "latestVersionCode" to android.defaultConfig.versionCode,
             "developer" to "github.com/timklge",
             "description" to "Open-source extension that provides an elevation graph datafield depicting the complete current route, including climbs and POIs (e. g. checkpoints and refueling stops). Also provides a minimap datafield and a POI navigation datafield to look up upcoming POIs of certain categories on the route (e. g. supermarkets).",
-            "releaseNotes" to "* Add remaining distance on gravel data field\n* Play notification sound and POI name TTS over BT when showing approach alert",
+            "releaseNotes" to "* Show elevation in the profile's elevation unit; fix imperial Y-axis labels (thx @Aryeh95!)\n* Add remaining distance on gravel data field\n* Play notification sound and POI name TTS over BT when showing approach alert",
             "screenshotUrls" to listOf(
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/horizontal_routegraph.png",
                 "https://github.com/timklge/karoo-routegraph/releases/latest/download/minimap.png",
